2a09e5d2d70128d93a20c13230f076078d4b2c5e
Christian Andreas Lærum-Onsager <christian.lerum-onsager@bouvet.no>
2020-06-09 11:51:14 +0200

    Add identity controllers & handlers

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Errors;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
@@ -12,7 +13,7 @@ namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -27,17 +28,19 @@ namespace Application.User
             }
         }
 
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
             private readonly SignInManager<AppUser> _signInManager;
             private readonly UserManager<AppUser> _userManager;
-            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+            private readonly IJWTGenerator _jwtGenerator;
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IJWTGenerator jwtGenerator)
             {
+                _jwtGenerator = jwtGenerator;
                 _userManager = userManager;
                 _signInManager = signInManager;
             }
 
-            public async Task<AppUser> Handle(Query request,
+            public async Task<User> Handle(Query request,
             CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
@@ -50,7 +53,13 @@ namespace Application.User
                 if (result.Succeeded)
                 {
                     // TODO: generate token
-                    return user;
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = _jwtGenerator.CreateToken(user),
+                        Username = user.UserName,
+                        Image = null
+                    };
                 }
                 else
                 {
---
+++ Application/User/Register.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using Application.Validators;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.User
+{
+    public class Register
+    {
+        public class Command : IRequest<User>
+        {
+            public string DisplayName { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(user => user.DisplayName).NotEmpty();
+                RuleFor(user => user.Username).NotEmpty();
+                RuleFor(user => user.Email).NotEmpty().EmailAddress();
+                RuleFor(user => user.Password).Password();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, User>
+        {
+            private readonly DataContext _context;
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJWTGenerator _jwtGenerator;
+            public Handler(DataContext context, UserManager<AppUser> userManager, IJWTGenerator jwtGenerator)
+            {
+                _jwtGenerator = jwtGenerator;
+                _userManager = userManager;
+                _context = context;
+            }
+
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            {
+                if (await _context.Users.AnyAsync(user => user.Email == request.Email))
+                    throw new RestException(HttpStatusCode.BadRequest, new { Email = "Email already exists" });
+                if (await _context.Users.AnyAsync(user => user.UserName == request.Username))
+                    throw new RestException(HttpStatusCode.BadRequest, new { Username = "Username already exists" });
+
+                var user = new AppUser
+                {
+                    DisplayName = request.DisplayName,
+                    Email = request.Email,
+                    UserName = request.Username,
+                };
+
+                var result = await _userManager.CreateAsync(user, request.Password);
+
+                if (result.Succeeded) return new User
+                {
+                    DisplayName = user.DisplayName,
+                    Username = user.UserName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    Image = null
+                };
+
+                throw new Exception("Problem creating user");
+            }
+        }
+    }
+}
